736e54d025c3407bcf83107cb9aa897d482925cb
brunadafonseca <brunadafonseca@gmail.com>
2019-03-17 20:26:44 +0100

    feat: update actions

--- src/actions/projects.js
+++ src/actions/projects.js
@@ -1,33 +1,33 @@
 
 import ApiClient from '../api/client'
 
-export const FETCHED_PROJECTS = 'FETCHED_PROJECTS'
+export const FETCHED_ALL_PROJECTS = 'FETCHED_ALL_PROJECTS'
 export const FETCHED_PROJECT = 'FETCHED_PROJECT'
 export const CREATED_PROJECT = 'CREATED_PROJECT'
 export const DELETED_PROJECT = 'DELETED_PROJECT'
 
 const api = new ApiClient()
 
-export const fetchProjects = () => {
+export const fetchAllProjects = () => {
   return (dispatch) => {
     api.get('/projects')
       .then((res) => {
         dispatch({
-          type: FETCHED_PROJECTS,
-          payload: res.body
+          type: FETCHED_ALL_PROJECTS,
+          payload: res.data
         })
       })
       .catch((error) => console.log(error.message))
   }
 }
 
-export const fetchProjectById = (id) => {
+export const fetchProject = (id) => {
   return dispatch => {
     api.get(`/projects/${id}`)
     .then((res) => {
         dispatch({
           type: FETCHED_PROJECT,
-          payload: res.body
+          payload: res.data
         })
       })
       .catch((error) => console.log(error.message))
@@ -53,7 +53,7 @@ export const deleteProject = (id) => {
       .then((res) => {
         dispatch({
           type: DELETED_PROJECT,
-          payload: res.body
+          payload: res.data
         })
       })
       .catch((error) => console.log(error.message))
--- src/actions/tiles.js
+++ src/actions/tiles.js
@@ -1,17 +1,17 @@
 
 import ApiClient from '../api/client'
 
-export const CREATED_TILE = 'CREATED_TILE'
+export const ADDED_TILE = 'ADDED_TILE'
 export const DELETED_TILE = 'DELETED_TILE'
 
 const api = new ApiClient()
 
-export const createTile = (id, newTile) => {
+export const addTile = (id, newTile) => {
   return (dispatch) => {
-    api.post(`/projects/${id}/tiles/`, newTile)
+    api.post(`/projects/${id}/tiles`, newTile)
       .then((res) => {
         dispatch({
-          type: CREATED_TILE,
+          type: ADDED_TILE,
           payload: res.data
         })
       })
@@ -25,7 +25,7 @@ export const deleteTile = (id, tileId) => {
       .then((res) => {
         dispatch({
           type: DELETED_TILE,
-          payload: res.body
+          payload: res.data
         })
       })
       .catch((error) => console.log(error.message))
