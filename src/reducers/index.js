6c3336dfd7b3b1d387e0849281c1d12863672e33
brunadafonseca <brunadafonseca@gmail.com>
2019-03-18 03:00:22 +0100

    feat: add more cases to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,67 @@
-import { FETCHED_PROJECTS, FETCHED_PROJECT, CREATED_PROJECT } from "../actions/project";
+import { FETCHED_ALL_PROJECTS, FETCHED_PROJECT, CREATED_PROJECT, DELETED_PROJECT } from "../actions/projects";
+import { ADDED_TILE, UPDATED_TILE, DELETED_TILE } from '../actions/tiles'
 
 const initialState = {
   projects: [],
   currentProject: {},
+  projectPhases: [
+    { title: 'Flowcharts', id: 1 },
+    { title: 'Wireframes', id: 2 },
+    { title: 'Prototype', id: 3 },
+    { title: 'Development', id: 4 },
+    { title: 'Test', id: 5 },
+    { title: 'Launch', id: 6 },
+  ]
 }
 
 export default function(state = initialState, { type, payload }) {
   switch (type) {
-    case FETCHED_PROJECTS:
+    case FETCHED_ALL_PROJECTS:
       return {
         ...state,
         projects: [...state.projects, ...payload],
-        }
+        currentProject: payload[0] || [],
+      }
 
     case FETCHED_PROJECT:
       return {
         ...state,
         currentProject: payload,
-        }
+      }
 
     case CREATED_PROJECT:
-    console.log(payload)
-        return {
-          ...state,
-          projects: [...state.projects, payload],
-        }
+      return {
+        ...state,
+        projects: [payload, ...state.projects],
+      }
+
+    case DELETED_PROJECT:
+      return {
+        ...state,
+        projects: state.projects.filter(project => project._id !== payload._id),
+        currentProject: state.projects[0],
+      }
+
+    case ADDED_TILE:
+      return {
+        ...state,
+        projects: [...state.projects, payload],
+        currentProject: payload,
+      }
+
+    case UPDATED_TILE:
+      return {
+        ...state,
+        projects: [...state.projects, payload],
+        currentProject: payload,
+      }
+
+    case DELETED_TILE:
+      return {
+        ...state,
+        projects: [...state.projects, payload],
+        currentProject: payload,
+      }
 
     default:
       return state
