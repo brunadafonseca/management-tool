7b5c8055e0e2fbcd1405b61b84f8876c71b36207
brunadafonseca <brunadafonseca@gmail.com>
2019-03-18 03:03:54 +0100

    feat: create components index

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import AddTileForm from './AddTileForm'
+import CreateProjectForm from './CreateProjectForm'
+import Sidebar from './Sidebar'
+import TileCard from './TileCard'
+
+export {
+  AddTileForm,
+  CreateProjectForm,
+  Sidebar,
+  TileCard,
+}
