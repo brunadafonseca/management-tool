ec14b31bb0485df0ce38f4824767ead63d80bf8d
brunadafonseca <brunadafonseca@gmail.com>
2019-03-18 02:57:48 +0100

    feat: import main component, add root styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Project } from './containers'
 import './App.scss'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <Project />
       </div>
     )
   }
